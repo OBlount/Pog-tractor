1308d11a799d12cf63c2b023b67ed3dc9c05b344
BuildTools <oblount077@gmail.com>
2021-02-21 21:58:16 +0000

    :zap: Basic listening to Twitch.tv chat

--- src/pog-tractor.ts
+++ src/pog-tractor.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import { ChatUserstate, Client } from "tmi.js";
+
+const tmi = require('tmi.js');
+
+const client:Client = new tmi.Client({
+	connection: { reconnect: true },
+	channels: []
+})
+
+client.connect();
+
+client.on('message', (channel:string, tags:ChatUserstate, message:string, self:boolean) => {
+	console.log(`${tags['display-name']}: ${message}`);
+})
