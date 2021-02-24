82e1476be5522b73f8972e8e3d7ad5e562e138b7
BuildTools <oblount077@gmail.com>
2021-02-24 23:22:33 +0000

    :heavy_plus_sign: - Created a vue client frontend with 'vue create -n client'

---
+++ Client/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home">
+        <h1>Pog-tractor: Test Landing Page</h1>
+        <img alt="Vue logo" src="../assets/logo.jpg">
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+
+export default class Home extends Vue {}
+</script>
